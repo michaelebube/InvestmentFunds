7db91390d5fba6c89d3175892d0fb1db3a151bad
michaelebube <michaelebube18@gmail.com>
2025-05-12 09:42:45 +0100

    new

--- src/components/BaseLoader.vue
+++ src/components/BaseLoader.vue
@@ -1,7 +1,7 @@
 <template>
   <div v-if="show" class="text-center py-8" v-bind="$attrs">
     <div
-      class="animate-spin rounded-full h-10 w-10 border-b-2 border-blue-500 mx-auto mb-4"
+      class="animate-spin rounded-full h-11 w-11 border-b-2 border-blue-500 mx-auto mb-4"
     ></div>
     <slot></slot>
   </div>
