9c560f769590f40e77a1658f1e5ae49375b4a348
michaelebube <michaelebube18@gmail.com>
2025-05-13 09:32:07 +0100

    Fixed logo issue

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -3,18 +3,13 @@
     class="flex bg-white fixed shadow-md items-center w-full h-[70px] sm:h-[75px] lg:h-[90px] z-10"
   >
     <div
-      class="mx-[20px] sm:mx-[40px] md:mx-[30px] xl:mx-[90px] 2xl:mx-[200px]"
+      class="mx-[20px] sm:mx-[40px] md:mx-[30px] xl:mx-[90px] 2xl:mx-[200px] flex items-center"
     >
-      <a
-        href="https://cowrywise.com"
-        target="_blank"
-        rel="noopener noreferrer"
-        class="inline-flex"
-      >
+      <a href="https://cowrywise.com" target="_blank" rel="noopener noreferrer">
         <img
           src="/svg/cowrywise.svg"
           alt="Cowrywise Logo"
-          class="h-[60px] sm:h-[65px] lg:h-[135px] aspect-square object-contain"
+          class="h-[25px] sm:h-[30px] object-contain"
         />
       </a>
     </div>
